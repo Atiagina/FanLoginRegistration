075506cde5bfe6544777df0571c5f9e3c42aeab5
Anna <atiagina@gmail.com>
2016-03-09 12:18:09 -0800

    Adds FanLogin and FanRegistration Methods

---
+++ App_Code/FanLoginRegistrationService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "FanLoginRegistrationService" in code, svc and config file together.
+public class FanLoginRegistrationService : IFanLoginRegistrationService
+{
+    ShowTrackerEntities ste = new ShowTrackerEntities();
+	
+        public int FanRegistration(FanLite f){
+
+            FanLogin fl = new FanLogin();
+            int result = ste.usp_RegisterFan(f.FanName, f.FanEmail, f.FanUsername, f.FanPassword);
+            return result;
+        }
+
+        public int FanLogin(string username, string password)
+        {
+            int result = ste.usp_FanLogin(username, password);
+
+            if (result != -1)
+            {
+                var key = from k in ste.FanLogins
+                          where k.FanLoginUserName.Equals(username)
+                          select new {k.FanLoginKey};
+
+                          foreach (var k in key){
+
+                              result = (int)k.FanLoginKey;
+                          }
+
+            }
+            return result;
+        }
+	}
+
---
+++ App_Code/IFanLoginRegistrationService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IFanLoginRegistrationService" in both code and config file together.
+[ServiceContract]
+public interface IFanLoginRegistrationService
+{
+    [OperationContract]
+    int FanRegistration(FanLite f);
+
+    [OperationContract]
+    int FanLogin(string username, string password);
+
+}
+
+[DataContract]
+
+public class FanLite{
+
+    [DataMember]
+    public string FanName { set; get; }
+
+    [DataMember]
+    public string FanEmail { set; get; }
+
+    [DataMember]
+    public string FanUsername { set; get; }
+
+    [DataMember]
+    public string FanPassword { set; get; }
+}
